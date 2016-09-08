f3556ebd270fe45ba7f032dfcc1d652ffff25955
Jacob Ethier <jethier@theorymac1.jlab.org>
2016-09-08 15:34:16 -0400

    added fortran FF codes

---
+++ fortran/example.f
@@ -0,0 +1,18 @@
+***************************************************************************                                                                                   
+      PROGRAM JAM16FF
+      IMPLICIT NONE
+      INTEGER ipos
+      CHARACTER(len=20)::flav,hadron
+      REAL*8 z,Q2,dp,get_zF
+
+      ipos=0
+      flav='dp'
+      hadron='pion'
+      z=0.5
+      Q2=10.0
+
+      print *, get_zF(ipos,hadron,flav,z,Q2)
+
+      END PROGRAM
+
+**************************************************************************                                                                                    
