26acbb5685b0523775fe033e5290175040323853
Jacob Ethier <jethier@theorymac1.jlab.org>
2016-09-16 11:36:43 -0400

    updated example.f

--- fortran/example.f
+++ fortran/example.f
@@ -1,7 +1,7 @@
 ***************************************************************************                                                                                   
       PROGRAM EXAMPLE
       IMPLICIT NONE
-      INTEGER ipos
+      INTEGER ipos,i
       CHARACTER flav*2,lib*10,dist*10
       REAL*8 x,Q2,up,get_xF
 
@@ -13,9 +13,20 @@
       Q2=10.0
       flav='up'
 
+      !! To obtain value from single posterior:
       CALL GRID_INIT(lib,dist,ipos)
       up = get_xF(x,Q2,flav)
       print *, up
+
+      !!To obtain average value:
+      up = 0.D0
+      DO i=0,199
+         CALL GRID_INIT(lib,dist,i)
+         up=up+get_xF(x,Q2,flav)  !!Sum values from each posterior
+      ENDDO
+      up = up/200.D0            !!Divide by total number of posteriors
+
+      print *,up
 
       END PROGRAM
 
