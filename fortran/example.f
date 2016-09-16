b9e349947d3bc80fa26f47c5fb1c723f0d329aee
Jacob Ethier <jethier@theorymac1.jlab.org>
2016-09-16 11:48:55 -0400

    updated example

--- fortran/example.f
+++ fortran/example.f
@@ -5,8 +5,12 @@
       CHARACTER flav*2,lib*10,dist*10
       REAL*8 x,Q2,up,get_xF
 
-      lib='JAM15'
-      dist='PPDF'
+      !!Current flag choices (as of 09/16/16):
+      !! For lib='JAM15': dist='PPDF','T3PPDF','T4PPDF'
+      !! For lib='JAM16': dist='FFpion','FFkaon'
+
+      lib='JAM16'
+      dist='FFpion'
       ipos=0
 
       x=0.5
@@ -16,7 +20,7 @@
       !! To obtain value from single posterior:
       CALL GRID_INIT(lib,dist,ipos)
       up = get_xF(x,Q2,flav)
-      print *, up
+      print *, 'xF(z=0.5,Q2=10) from single posterior (ipos=0):',up
 
       !!To obtain average value:
       up = 0.D0
@@ -26,7 +30,7 @@
       ENDDO
       up = up/200.D0            !!Divide by total number of posteriors
 
-      print *,up
+      print *,'Average xF(z=0.5,Q2=10):',up
 
       END PROGRAM
 
