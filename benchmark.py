f4d5cd99013e7998fa5fb5ac46766557c2ad3663
Alberto <accardi@jlab.org>
2016-11-02 16:14:08 -0400

    AA small mods

--- benchmark.py
+++ benchmark.py
@@ -4,12 +4,12 @@ import numpy as np
 import pylab as py
 from python.corelib import JAMLIB
 import lhapdf
-from fortran import flib
+#from fortran import flib
 
 P1=JAMLIB('python/JAM16/FFpion')
 P2 = lhapdf.mkPDF("JAM16_FF_pi_Ceven",1)
 #P3 = flib.grid_init('fortran/JAM16','FFpion',0)
-flib.grid_init('JAM16','FFpion',0)
+#flib.grid_init('JAM16','FFpion',0)
 
 x=0.01
 Q2=10.0
@@ -18,7 +18,7 @@ print 'x=',x
 print 'Q2=',Q2
 print 'python  = ',P1.get_XF(0,'up',x,Q2)
 print 'lhadpf  = ',P2.xfxQ2(901,x,Q2)
-print 'fortran = ',flib.get_xf(x,Q2,'up')
+#print 'fortran = ',flib.get_xf(x,Q2,'up')
 
 
 x=0.9
@@ -28,7 +28,7 @@ print 'x=',x
 print 'Q2=',Q2
 print 'python  = ',P1.get_XF(0,'up',x,Q2)
 print 'lhadpf  = ',P2.xfxQ2(901,x,Q2)
-print 'fortran = ',flib.get_xf(x,Q2,'up')
+#print 'fortran = ',flib.get_xf(x,Q2,'up')
 
 
 x=1.0
@@ -38,7 +38,7 @@ print 'x=',x
 print 'Q2=',Q2
 print 'python  = ',P1.get_XF(0,'up',x,Q2)
 print 'lhadpf  = ',P2.xfxQ2(901,x,Q2)
-print 'fortran = ',flib.get_xf(x,Q2,'up')
+#print 'fortran = ',flib.get_xf(x,Q2,'up')
 
 
 
